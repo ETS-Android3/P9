a55c85842378062f893c076b495d5d58108ff11e
Guillaume Toussaint <gtdev.git@gmail.com>
2021-09-30 16:09:13 +0200

    add location permission activity and insert GOOGLE_API_KEY loaded from gradle.properties

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainApplication.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainApplication.java
@@ -1,18 +1,27 @@
 package com.openclassrooms.realestatemanager;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.openclassrooms.realestatemanager.tag.Tag;
 
 public class MainApplication extends Application {
 
     private static Application application;
+    private static String googleApiKey;
 
     @Override
     public void onCreate() {
         super.onCreate();
         application = this;
+        googleApiKey = com.openclassrooms.realestatemanager.BuildConfig.GOOGLE_PLACES_KEY;
+        Log.d(Tag.TAG, "MainApplication.onCreate() called googleApiKey=\"" + googleApiKey +"\"");
     }
 
     public static Application getApplication() {
         return application;
+    }
+    public static String getGoogleApiKey() {
+        return googleApiKey;
     }
 }
---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/launcher/NavigationActivity.java
@@ -0,0 +1,38 @@
+package com.openclassrooms.realestatemanager.ui.launcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.openclassrooms.realestatemanager.ui.MainActivity;
+import com.openclassrooms.realestatemanager.ui.permission.PermissionActivity;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Intent intent;
+
+        // if not permission ask for permission
+        if (ContextCompat.checkSelfPermission(NavigationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            intent = new Intent(NavigationActivity.this, PermissionActivity.class);
+            startActivity(intent);
+        }
+
+        // if permission ok check how to open application
+        if (ContextCompat.checkSelfPermission(NavigationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // open main
+            intent = new Intent(NavigationActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        finish();
+    }
+}
