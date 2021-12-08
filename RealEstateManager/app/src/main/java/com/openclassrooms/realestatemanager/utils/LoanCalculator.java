c73116a81f1cfc4bd2fad2cd4fe75a8411c05ae5
Guillaume Toussaint <gtdev.git@gmail.com>
2021-12-08 14:28:40 +0100

    create LoanCalculator and his unit test

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/utils/LoanCalculator.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.realestatemanager.utils;
+
+public class LoanCalculator {
+
+    /**
+     * calculate monthly payment
+     * @param amount
+     * @param rate (annual rate)
+     * @param duration (duration in month)
+     * @return
+     */
+    public double calculateMonthlyPayment(float amount, double rate, int duration){
+
+
+        /*
+            mp = monthly payment
+            pr = periodic Rate
+            a =  amount
+            d = duration in month
+
+        pr = rate / 12
+
+                                 d
+                a * pr * (1 + pr)
+        mp =  -----------------------------------
+                        d
+                (1 + pr)  -1
+        */
+
+        if (amount == 0)
+            return 0;
+        if (duration == 0)
+            return amount;
+        if (rate == 0)
+            return amount / duration;
+
+        double pr = rate / 12 / 100;
+        double monthlyPayment = (amount * pr * Math.pow(1 + pr, duration)) / (Math.pow(1 + pr, duration) - 1);
+        return monthlyPayment;
+    }
+}
