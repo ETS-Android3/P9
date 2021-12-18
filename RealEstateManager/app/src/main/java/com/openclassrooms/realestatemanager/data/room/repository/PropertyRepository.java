1984bfb40309d22c7a07ded52f47199c0b5483df
Guillaume Toussaint <gtdev.git@gmail.com>
2021-12-18 14:54:30 +0100

    remove warning

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/room/repository/PropertyRepository.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/room/repository/PropertyRepository.java
@@ -34,51 +34,29 @@ public class PropertyRepository {
     // return a long. This is the newly generated ID
     public long insert(Property property) throws ExecutionException, InterruptedException {
         Log.d(Tag.TAG, "insert() called with: property = [" + property + "]");
-        Callable<Long> callable = new Callable<Long>() {
-            @Override
-            public Long call() throws Exception {
-                return propertyDao.insert(property);
-            }
-        };
+        Callable<Long> callable = () -> propertyDao.insert(property);
 
         Future<Long> future = AppDatabase.getExecutor().submit(callable);
-        Long id = future.get();
 
-        return  id;
+        return future.get();
     }
 
     // Return the number of updated rows
     public int update(Property property) throws ExecutionException, InterruptedException {
         Log.d(Tag.TAG, "update() called with: property = [" + property + "]");
-        Callable<Integer> callable = new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                return propertyDao.update(property);
-            }
-        };
+        Callable<Integer> callable = () -> propertyDao.update(property);
 
         Future<Integer> future = AppDatabase.getExecutor().submit(callable);
-        int count = future.get();
 
-        return count;
+        return future.get();
     }
 
     public void delete(long id) {
-        AppDatabase.getExecutor().execute(() -> {
-            propertyDao.delete(id);
-        });
-    }
-
-    public LiveData<List<PropertyLocationData>> getOtherPropertiesLocationById(long id){
-        return propertyDao.getOtherPropertiesLocationById(id);
+        AppDatabase.getExecutor().execute(() -> propertyDao.delete(id));
     }
 
     public LiveData<PropertyDetailData> getPropertyDetailByIdLiveData(long id) {
         return propertyDao.getPropertyDetailById(id);
-    }
-
-    public LiveData<PropertyLocationData> getPropertyLocationById(long id){
-        return propertyDao.getPropertyLocationById(id);
     }
 
     public LiveData<List<PropertyLocationData>> getPropertiesLocation(){
@@ -95,42 +73,31 @@ public class PropertyRepository {
 
     public LiveData<List<Property>> getPropertiesWithFilterLiveData(SimpleSQLiteQuery query) {
         return propertyDao.getPropertiesWithFilterLiveData(query);
-    };
+    }
 
     public List<Property> getPropertiesWithFilter(SimpleSQLiteQuery query){
-        Callable<List<Property>> callable = new Callable<List<Property>>() {
-            @Override
-            public List<Property> call() throws Exception {
-                return propertyDao.getPropertiesWithFilter(query);
-            }
-        };
+        Callable<List<Property>> callable = () -> propertyDao.getPropertiesWithFilter(query);
 
         List<Property> properties = new ArrayList<>();
         Future<List<Property>> future = AppDatabase.getExecutor().submit(callable);
         try {
             List<Property> list = future.get();
             properties.addAll(list);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
 
         return properties;
     }
 
-    private MutableLiveData<PropertySearchParameters> propertySearchParametersMutableLiveData = new MutableLiveData<>();
+    private final MutableLiveData<PropertySearchParameters> propertySearchParametersMutableLiveData = new MutableLiveData<>();
     public void setPropertySearchParameters(PropertySearchParameters propertySearchParameters) {
         propertySearchParametersMutableLiveData.setValue(propertySearchParameters);
     }
 
-    public void resetSearch(){
-        setPropertySearchParameters(new PropertySearchParameters());
-    }
-
     public LiveData<List<Property>> getProperties() {
         return Transformations.switchMap(propertySearchParametersMutableLiveData,
-            propertySearchParameters -> {return getPropertiesWithFilterLiveData(propertySearchParameters.getQuery());});
+            propertySearchParameters -> getPropertiesWithFilterLiveData(propertySearchParameters.getQuery()));
     }
 
     public LiveData<Long> getFirstOrValidIdLiveData(Long id) {
@@ -146,7 +113,7 @@ public class PropertyRepository {
                             }
                         }
                     }
-                    return new Long(result);
+                    return result;
                 });
     }
 
