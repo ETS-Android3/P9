a12d5ca3e1545655a563a9ae0238a886a09af064
Guillaume Toussaint <gtdev.git@gmail.com>
2021-12-18 15:04:10 +0100

    remove unused context

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/propertylist/view/PropertyListAdapter.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/propertylist/view/PropertyListAdapter.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.realestatemanager.ui.propertylist.view;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
